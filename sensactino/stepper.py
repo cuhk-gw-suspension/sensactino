043f09ac824902a473ae566060438bbf2bc8fc33
Lam Yee Ching <lamyc627@gmail.com>
2021-08-04 15:46:14 +0800

    prototype

--- sensactino/stepper.py
+++ sensactino/stepper.py
@@ -2,6 +2,8 @@ import serial
 import time
 import numpy as np
 
+from .core.utils import _send_command
+
 class Stepper:
     def __init__(self, port, baudrate, timeout=1):
         """Save configuration of the serial communication to the stepper.
@@ -53,12 +55,12 @@ class Stepper:
             #        b'\n']
             # cmd = b"".join(cmd)
             cmd = "M" + str(position) + "\n"
-            self._send_command(cmd)
+            _send_command(self.command, cmd)
         else:
             raise TypeError("position must be of type int.")
 
     def move(self, displacement):
-        """Command the stepper to move displacement relative to 
+        """Command the stepper to move displacement relative to
         the current position. Positive direction is indicated by DIR_pin=LOW.
         Origin is the position where the stepper is initialized.
 
@@ -76,24 +78,10 @@ class Stepper:
             #        b'\n']
             # cmd = b"".join(cmd)
             cmd = "S" + str(displacement) + "\n"
-            self._send_command(cmd)
+            _send_command(self.serial, cmd)
         else:
             raise TypeError("displacement must be of type int.")
 
-    def _send_command(self, cmd):
-        """Handle the byte conversion and send the command to the stepper.
-
-        Parameters
-        ----------
-        cmd : str-like
-            command to be sent to the stepper.
-        """
-        if isinstance(cmd, str):
-            cmd = cmd.encode("ascii")
-
-        if not isinstance(cmd, (bytes, bytearray)):
-            raise TypeError("cmd must be string-like object")
-        self.serial.write(cmd)
 
     # def reset(self):
     #     """Move the stepper to the center of the rail.
@@ -102,27 +90,4 @@ class Stepper:
     #     cmd = cmd.encode("ascii")
     #     self.serial.write(cmd)
 
-    def sweep(self, position, fs):
-        """Command the stepper to move to the positions consecutively at
-        constant frequency fs.
-
-        Parameters
-        ----------
-        position : int array_like
-            Target position of the stepper, in steps.
-
-        """
-        dt = 1/fs
-        position = np.array(position)
-        timeold = time.perf_counter()
-        for i in range(len(position)):
-            self.moveto(position[i])
-
-            time_not_passed = (time.perf_counter() - timeold < dt)
-            while time_not_passed:
-                time.sleep(1e-8)
-                timenew = time.perf_counter()
-                time_not_passed = (timenew - timeold < dt)
-            timeold = timenew
-        print("sweep finished.")
 
